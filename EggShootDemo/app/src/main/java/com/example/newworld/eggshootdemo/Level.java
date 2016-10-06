c520c5ccfd4297bbc1b67882043d8151d2583390
Quốc Việt <vuongquocviet1996@gmail.com>
2016-10-06 19:50:12 +0700

    Update
    Tạo giao diện, controll cho các layout ! Thiểu controll play !

--- EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Level.java
+++ EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Level.java
@@ -1,15 +1,57 @@
 package com.example.newworld.eggshootdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
 /*
 Hiển thị giao diện chọn độ khó
  */
 public class Level extends AppCompatActivity {
+    private Button buttonDe;
+    private Button buttonVua;
+    private Button buttonKho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.level);
+
+        buttonDe = (Button) findViewById(R.id.button_De);
+        buttonVua = (Button) findViewById(R.id.button_Vua);
+        buttonKho = (Button) findViewById(R.id.button_Kho);
+
+        buttonDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play = new Intent(Level.this,Play.class);
+                startActivity(play);
+            }
+        });
+
+        buttonVua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play = new Intent(Level.this,Play.class);
+                startActivity(play);
+            }
+        });
+
+        buttonKho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play = new Intent(Level.this,Play.class);
+                startActivity(play);
+            }
+        });
     }
 }
--- EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Setting.java
+++ EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Setting.java
@@ -2,6 +2,9 @@ package com.example.newworld.eggshootdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 /*
 Hiển thị giao diện cài đặt
  */
@@ -10,6 +13,11 @@ public class Setting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.setting);
     }
 }
