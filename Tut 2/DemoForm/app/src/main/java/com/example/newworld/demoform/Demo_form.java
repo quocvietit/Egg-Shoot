cb76a5eb961bb2f41c02a7ad7784e4ed3bd60d04
Quốc Việt <vuongquocviet1996@gmail.com>
2016-10-14 16:07:40 +0700

    Task 2: luu du lieu
    Task 2: luu du lieu

--- Tut 2/DemoForm/app/src/main/java/com/example/newworld/demoform/Demo_form.java
+++ Tut 2/DemoForm/app/src/main/java/com/example/newworld/demoform/Demo_form.java
@@ -1,7 +1,11 @@
 package com.example.newworld.demoform;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Demo_form extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class Demo_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_form);
+    }
+
+    SharedPreferences ref;
+    Button btnAdd;
+    Button btnPrint;
+    EditText txtName;
+    EditText txtId;
+
+    private void addEvent() {
+        txtName = (EditText) this.findViewById(R.id.txtName);
+        txtId = (EditText) this.findViewById(R.id.txtId);
+        btnAdd = (Button) this.findViewById(R.id.btnAdd);
+        btnPrint = (Button) this.findViewById(R.id.Print);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ref.edit().putString(txtId.getText().toString(), txtName.getText().toString()).commit();
+                txtId.setText("");
+                txtName.setText("");
+            }
+        });
     }
 }
