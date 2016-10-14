56a3ff1f3d5337db843e85fcb69f9613b1a08171
Quốc Việt <vuongquocviet1996@gmail.com>
2016-10-14 15:59:55 +0700

    Task1: giao dien don gian
    Task1: giao dien don gian

---
+++ Tut 2/DemoForm/app/src/main/java/com/example/newworld/demoform/Demo_form.java
@@ -0,0 +1,13 @@
+package com.example.newworld.demoform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Demo_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_form);
+    }
+}
