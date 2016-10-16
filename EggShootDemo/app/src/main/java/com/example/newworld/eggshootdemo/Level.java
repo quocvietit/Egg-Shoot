6dce02659a2488c191d03e257f212acc8644b2b4
datnguyen31296 <dat312996@gmail.com>
2016-10-16 15:08:57 +0700

    coderun

--- EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Level.java
+++ EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Level.java
@@ -1,57 +1,80 @@
 package com.example.newworld.eggshootdemo;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.view.View;
 
 /*
 Hiển thị giao diện chọn độ khó
  */
-public class Level extends AppCompatActivity {
-    private Button buttonDe;
-    private Button buttonVua;
-    private Button buttonKho;
-
+public class Level extends Activity {
+     Button buttonDe;
+     Button buttonVua;
+     Button buttonKho;
+    Play play;
+    private static final String TAG = Level.class.getSimpleName();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        public void onCreate(Bundle savedInstanceState) {
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.level);
+            super.onCreate(savedInstanceState);
+            Map.initMaps();
+            play = new Play(this);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.level);
 
         buttonDe = (Button) findViewById(R.id.button_De);
         buttonVua = (Button) findViewById(R.id.button_Vua);
         buttonKho = (Button) findViewById(R.id.button_Kho);
 
+
         buttonDe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent play = new Intent(Level.this,Play.class);
-                startActivity(play);
+                Play.levelN=1;
+                Play.reInit();
+                setContentView(play);
             }
         });
 
         buttonVua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent play = new Intent(Level.this,Play.class);
-                startActivity(play);
+                Play.levelN=2;
+                Play.reInit();
+                setContentView(play);
             }
         });
 
         buttonKho.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent play = new Intent(Level.this,Play.class);
-                startActivity(play);
+                Play.levelN=3;
+                Play.reInit();
+                setContentView(play);
             }
         });
+        //        setContentView(new GameScreen(this));
+
+
+        Log.d(TAG, "View added");
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Destroying...");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping...");
+        super.onStop();
     }
 }
---
+++ EggShootDemo/app/src/main/java/com/example/newworld/eggshootdemo/Map.java
@@ -0,0 +1,22 @@
+package com.example.newworld.eggshootdemo;
+
+/**
+ * Created by admin on 10/15/2016.
+ */
+
+public class Map {
+    static int [][]Maps;
+    static void initMaps(){
+        Maps = new int[4][];
+        int []map0={0,0,1,1,2,2,3,3,0,0,1,1,2,2,3,2,2,3,3};
+        int []map1={0,0,1,1,2,2,3,3,0,0,1,1,2,2,3,2,2,3,3,0,0,1,1,2,3,3,0,0,1,1};
+        int []map2={2,2,2,2,2,2,2,2,
+                1,1,1,1,1,1,1,1,
+                5,5,5,5,5,5,5,5,
+                4,4,4
+        };
+        Maps[0]=map0;
+        Maps[1]=map1;
+        Maps[2]=map2;
+    }
+}
