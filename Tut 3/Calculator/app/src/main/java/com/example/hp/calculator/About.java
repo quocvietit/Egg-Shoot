ad8118457f1846810cbac28f94194bf1e84fa726
datnguyen31296 <dat312996@gmail.com>
2016-10-21 17:45:32 +0700

    Thuchanh_T6

---
+++ Tut 3/Calculator/app/src/main/java/com/example/hp/calculator/About.java
@@ -0,0 +1,24 @@
+package com.example.hp.calculator;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class About extends Activity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Button btnback = (Button) findViewById(R.id.back);
+        btnback.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+
+    }
+}
